a753bc5c741d071d683969fc81d0314d25a47c39
AndreyChim <chimitcyrenov@mail.ru>
2025-03-24 14:37:48 +0800

    Add actions & views

--- demo/app/controllers/users_controller.rb
+++ demo/app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: [:show, :edit, :update]
 
-    def index
-      @users = User.all
+  def index
+    @users = User.all
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit
     end
+  end
+
+   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
