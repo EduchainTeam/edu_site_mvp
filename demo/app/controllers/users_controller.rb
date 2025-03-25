316608a2362d3f7ff5f320d09ae8b0dd283eab37
AndreyChim <chimitcyrenov@mail.ru>
2025-03-25 18:03:34 +0800

    Add admin's flag

--- demo/app/controllers/users_controller.rb
+++ demo/app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :authorize_user, only: [:show, :edit, :update]
 
   def index
-    @users = User.all
+    if current_user.admin?
+      @users = User.all
+    else
+      redirect_to user_path(current_user), alert: "You are not authorized to view this page."
+    end
   end
 
   def update
@@ -20,7 +25,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def authorize_user
+    unless current_user.admin? || @user == current_user
+      redirect_to users_path, alert: "You are not authorized to perform this action."
+    end
+  end
+
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :admin)
   end
 end
